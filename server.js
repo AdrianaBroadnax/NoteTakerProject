109408fb92f8f6ff0d58be9ec328abd48b2747eb
Adriana Broadnax <alwaysadrianna@gmail.com>
2021-11-27 15:37:37 -0500

    server.js

--- server.js
+++ server.js
@@ -1,24 +1,31 @@
+// importing express package for the server framework
 const express = require('express');
+// importing file path resolution
 const path = require('path');
 const noteData = require('db.json');
 
+// creating out server object
+const app = express ();
+// defining the port variable
 const PORT = 3001;
-
-const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// determines the supporting assets are getting pulled from
 app.use(express.static('public'));
 
+// GET http://localhost:3001/
+app.get('/', (req,res) => res.send('/notes'));
+
+// GET http://localhost:3001/
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'notes.html'));
 });
 
 '/api/notes'
 app.get('/api/notes', (req, res) => res.json(noteData));
 
-app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
-  });
-  
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
